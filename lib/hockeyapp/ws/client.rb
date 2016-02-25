f23b4db0d5c8d2ce8575770870c917965aa6d677
chetan K S <chetan.ks@v2solutions.com>
2016-02-25 17:19:05 +0530

    updating to raise exception for files

--- lib/hockeyapp/ws/client.rb
+++ lib/hockeyapp/ws/client.rb
@@ -72,10 +72,10 @@ module HockeyApp
     def new_app title, bundle_id, options = {}
       accepted_formats = [".png", ".gif", ".jpeg"]
       icon_path = options[:icon]
-      unless !File.exists? icon_path
-        options[:icon] = File.open(icon_path, "rb") if accepted_formats.include? File.extname(icon_path)
-      end
-      resp = ws.create_new_app title, bundle_id, options
+      raise "Image not found at #{icon_path}" unless File.exists? icon_path
+      raise "Image format with #{File.extname(icon_path)} extension not supported" unless accepted_formats.include? File.extname(icon_path)
+      options[:icon] = File.open(icon_path, "rb")
+      resp = ws.create_new_app(title, bundle_id, options)
       raise resp['errors'].map{|e|e.to_s}.join("\n") unless resp['errors'].nil?
       App.from_hash(resp, self)
     end
